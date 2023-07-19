64deee9b9ed42515ca601a2cf018650c5ffe951a
Sam Mariassouce <sammariassouce@gmail.com>
2023-07-19 06:15:14 -1000

    stats

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItems={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -111,10 +111,25 @@ function Item({ item, onDeleteItem, onToggleItems }) {
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>start adding items to your packing list ✈️</em>
+      </p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>🧳 You have X items on your list and you already packed X (X%</em>)
+      <em>
+        {percentage === 100
+          ? "you've got everything packed. You are good to go 🚀"
+          : ` 🧳 You have {numItems} items on your list and you already packed
+        ${numPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
